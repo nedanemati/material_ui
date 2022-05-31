a8c23a7d7b75eccb573894efd49d0d124d113904
nedanemati <74539243+nedanemati@users.noreply.github.com>
2022-05-31 09:17:07 +0430

    feat:added customsoftware page

--- src/components/ui/CustomSoftware.jsx .jsx
+++ src/components/ui/CustomSoftware.jsx .jsx
@@ -1,4 +1,5 @@
 import documentsAnimations from "../../animations/documentsAnimation/data";
+import scaleAnimation from "../../animations/scaleAnimation/data.json";
 import Lottie from "react-lottie";
 import { Link } from "react-router-dom";
 import { makeStyles, useTheme } from "@mui/styles";
@@ -6,12 +7,16 @@ import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Hidden from "@mui/material/Hidden";
 
 import backArrow from "../../assets/backArrow.svg";
 import forwardArrow from "../../assets/forwardArrow.svg";
 import lightbulb from "../../assets/bulb.svg";
 import cash from "../../assets/cash.svg";
 import stopwatch from "../../assets/stopwatch.svg";
+import roots from "../../assets/root.svg";
+import automationAnimation from "../../animations/automationAnimation/data.json";
+import uxAnimation from "../../animations/uxAnimation/data";
 
 const useStyles = makeStyles((theme) => ({
   heading: {
@@ -33,10 +38,35 @@ const useStyles = makeStyles((theme) => ({
 
 function CustomSoftware(props) {
   const classes = useStyles();
+
   const documentsOptions = {
     loop: true,
     autoplay: false,
     animationData: documentsAnimations,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const scaleOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: scaleAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const automationOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: automationAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  const uxOptions = {
+    loop: true,
+    autoplay: false,
+    animationData: uxAnimation,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice",
     },
@@ -122,9 +152,10 @@ function CustomSoftware(props) {
           </Grid>
         </Grid>
       </Grid>
-      <Grid item container direction="row">
-        <Grid item container className={classes.itemContainer}>
-          <Grid item container direction="column">
+
+      <Grid item container direction="row" justifyContent="space-around">
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
             <Grid item>
               <Typography variant="h4">Digital Document</Typography>
             </Grid>
@@ -137,6 +168,104 @@ function CustomSoftware(props) {
               </Typography>
               <Typography variant="body1" paragraph>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, molestiae.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie
+              options={documentsOptions}
+              isStopped={true}
+              style={{ maxHeight: 275, maxWidth: 275, minHeight: 250 }}
+            />
+          </Grid>
+        </Grid>
+
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie options={scaleOptions} isStopped={true} style={{ maxHeight: 260, maxWidth: 280 }} />
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="center">
+                Scale{" "}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis reiciendis dolores repellendus ipsum?
+                Architecto ea voluptate distinctio dolorem at vel aliquam ratione eum, illum nisi repellendus mollitia,
+                quo quisquam quas dicta laboriosam fuga officiis commodi unde sequi quia? Cumque, laborum quis omnis
+                distinctio hic voluptas ratione corrupti molestiae dignissimos doloremque.
+              </Typography>
+            </Grid>
+          </Grid>
+          {/* <Grid item md></Grid> */}
+        </Grid>
+      </Grid>
+
+      <Grid item container direction="row" style={{ marginTop: "20em", marginBottom: "20em" }}>
+        <Grid item container direction="column" alignItems="center">
+          <Grid item>
+            <img src={roots} alt="tree whit roots extending out" height="450" width="450" />
+          </Grid>
+          <Grid item className={classes.itemContainer}>
+            <Typography variant="h4" align="center" gutterBottom>
+              Root Cause
+            </Typography>
+            <Typography variant="body1" align="center" paragraph>
+              Lorem ipsum dolor sit amet.
+            </Typography>
+            <Typography variant="body1" align="center" paragraph>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, voluptate! Rerum cumque non laboriosam
+              repellat hic nemo vel ea labore.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid item container direction="row" justifyContent="space-around" style={{ marginBottom: "10em" }}>
+        <Grid item container className={classes.itemContainer} md>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4">Automation</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" paragraph>
+                Lorem ipsum dolor sit amet.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipisicing.
+              </Typography>
+              <Typography variant="body1" paragraph>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore, molestiae.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item md>
+            <Lottie options={automationOptions} isStopped={true} style={{ maxHeight: 290, maxWidth: 280 }} />
+          </Grid>
+        </Grid>
+
+        <Grid item container className={classes.itemContainer}>
+          <Grid item md>
+            <Lottie options={uxOptions} isStopped={true} style={{ maxHeight: 310, maxWidth: 155 }} />
+          </Grid>
+          <Grid item container direction="column" md>
+            <Grid item>
+              <Typography variant="h4" align="center">
+                User Expreience Desgin
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body1" align="right" paragraph>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis reiciendis dolores repellendus ipsum?
+                Architecto ea voluptate distinctio dolorem at vel aliquam ratione eum, illum nisi repellendus mollitia,
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis reiciendis dolores repellendus ipsum?
+              </Typography>
+              <Typography variant="body1" align="right" paragraph>
+                Architecto ea voluptate distinctio dolorem at vel aliquam ratione eum, illum nisi repellendus mollitia,
               </Typography>
             </Grid>
           </Grid>
